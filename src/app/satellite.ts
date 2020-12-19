2ebf805fe10b9c1c07b2a50d88b6ab2a23fb2079
Asma Charef <asma.charef@hotmail.com>
2020-12-19 12:14:26 -0600

    completed

---
+++ src/app/satellite.ts
@@ -0,0 +1,22 @@
+export class Satellite {
+    name: string;
+    type: string;
+    operational: boolean;
+    orbitType: string;
+    launchDate: string;
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        this.name = name;
+        this.type = type;
+        this.operational = operational;
+        this.orbitType = orbitType;
+        this.launchDate = launchDate;
+    }
+    shouldShowWarning() {
+        if(this.type === "Space Debris") {
+            return true;
+        }
+        else {
+           return false;
+        } 
+    }
+}
